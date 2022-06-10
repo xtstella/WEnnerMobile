1d995a4c28d57e93dbca5870e2e41b28c27c1312
Tong XUE <xtstella@hotmail.com>
2022-06-10 13:08:36 +0200

    refactor: delete useless directory

--- oldVersion/CoachSport 0821/app/src/main/java/com/limsi/coachsport/User.java
+++
@@ -1,9 +0,0 @@
-package com.limsi.coachsport;
-
-/**
- * Created by user on 27-06-2016.
- */
-public class User {
-    byte[] myImageId;
-    String details;
-}
--- oldVersion/CoachSport 0830/app/src/main/java/com/limsi/coachsport/Activity.java
+++
@@ -1,8 +0,0 @@
-package com.limsi.coachsport;
-
-/**
- * Created by xt on 2017/8/23.
- */
-
-public class Activity {
-}
